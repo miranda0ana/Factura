985b3b595bdb901ff259488262a79c26cd861a9f
miranda0ana <miranda_mv_@hotmail.com>
2017-06-26 11:04:11 -0300

    modificamos y agregamos clases

--- src/factura/Factura.java
+++ src/factura/Factura.java
@@ -66,48 +66,5 @@ public class Factura {
     }
     
        
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        
-        Cliente unCliente = new Cliente();
-        unCliente.setNombre("Manuel");
-        
-        FormaCobro unFormaCobro = new FormaCobro();
-        unFormaCobro.setNombre("Visa");
-        
-        Item cocaCola = new Item();
-        cocaCola.setNombre("Coca cola");
-        cocaCola.setPrecio(12.43);
-
-        Item pepsi = new Item();
-        pepsi.setNombre("Pepsi");
-        pepsi.setPrecio(15.43);
-
-        
-        ItemFactura unItemFacturaPepsi = new ItemFactura();
-        unItemFacturaPepsi.setCantidad(2.0);
-        unItemFacturaPepsi.setItem_factura(pepsi);
-        unItemFacturaPepsi.setPrecio(pepsi.getPrecio());
-        
-        ItemFactura unItemFacturaCoca = new ItemFactura();
-        unItemFacturaCoca.setCantidad(10.0);
-        unItemFacturaCoca.setItem_factura(cocaCola);
-        unItemFacturaCoca.setPrecio(cocaCola.getPrecio());
-        
-        
-        
-        Factura unaFactura = new Factura();
-        unaFactura.setCliente(unCliente);
-        unaFactura.setNumero(2354);
-        unaFactura.setFormaCobro(unFormaCobro);
-        unaFactura.agregarItem(unItemFacturaPepsi);
-        unaFactura.agregarItem(unItemFacturaCoca);
-    
-        System.out.println(unaFactura.getItemFactura().get(0).getItem_factura().getPrecio());
-        
-        
-    }
     
 }
